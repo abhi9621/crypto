3e1e15dafb4313b44bd337ca1440043c5b87b191
abhi9621 <tewariabhishek9621@gmail.com>
2023-05-08 00:37:59 +0530

    first

---
+++ crpt/index.js
@@ -0,0 +1,48 @@
+
+var baseUrl = "https://api.coinranking.com/v2/coins";
+var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+var apiKey = "coinrankingccf6fb202c9b9d7d23274f740c4fc0e015e5cf3dae3f0032";
+// var proxyUrl="coinranking1.p.rapidapi.com"
+// var baseUrl="https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0"
+// var apiKey="613a9995b7mshaaeaabf7d41ec00p152cc2jsn283afafcc7a1"
+var apiUrl = `${proxyUrl}${baseUrl}`;
+console.log(apiUrl);
+
+fetch(`${proxyUrl}${baseUrl}`, { 
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-My-Custom-Header': `${apiKey}`,
+      'Access-Control-Allow-Origin': "*"
+    }
+})
+  .then((response) => {
+    if (response.ok) {
+      response.json().then((json) => {
+        console.log(json.data);
+        let coinsData = json.data.coins;
+
+        if (coinsData.length > 0) {
+          var cryptoCoin = "";
+        }
+        //For Loop Starts
+        coinsData.forEach((coin) => {
+          cryptoCoin += "<tr>";
+          cryptoCoin += `<td> ${coin.symbol} </td>`;
+          cryptoCoin += `<td> ${coin.uuid} </td>`;
+          cryptoCoin += `<td> ${coin.btcPrice} </td>`;
+          cryptoCoin += `<td> ${coin.rank}</td>`;
+          cryptoCoin += `<td> ${coin.tier} </td>`;      
+          cryptoCoin += `<td> <a href='http://127.0.0.1:5500/crpt/search.html?q=${coin.name}'> ${coin.name} </a> </td>`;
+          cryptoCoin += `<td> $${Math.round(coin.price)} Billion</td>`;
+          cryptoCoin += `<td> ${coin.symbol}</td>`;"<tr>";
+        });
+        //For Loop Ends
+        document.getElementById("data").innerHTML = cryptoCoin;
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+ 
---
+++ crpt/search.js
@@ -0,0 +1,59 @@
+
+document.addEventListener("click", searchCoin);
+async function searchCoin() {
+    const url = window.location.href;
+    console.log(url);
+    const urlObj = new URL(url);
+    console.log(urlObj);
+    const params = new URLSearchParams(urlObj.search);
+    console.log(params);
+    if (!params.has('q')) {
+        return;
+    }
+
+    let searchedText = params.get('q');
+    document.getElementsByName('q')[0].value = searchedText;
+    let request = await fetch(
+      `https://api.coingecko.com/api/v3/search?query=${searchedText}`
+    );
+    let data = await request.json();
+    render(data);
+    console.log(data);
+}
+function render(data) {
+    data.coins.forEach(({thumb,name,symbol,id},idx) => createCard(thumb,name,symbol,id,idx+1))
+    }
+    
+    function createCard(logo, name, symbol, coinId,idx) {
+        const indexEle = document.createElement("p");
+        indexEle.innerText = idx;
+    
+        const logoEle = document.createElement("img");
+        logoEle.src = logo;
+        logoEle.alt = "coin logo"
+        
+        const nameEle = document.createElement("h3");
+        nameEle.innerText = name;
+    
+        const shortNameEle = document.createElement("h3");
+        shortNameEle.innerText = symbol;
+    
+        const moreInfoEle = document.createElement('a');
+        moreInfoEle.innerText = 'Graph representaion';
+        moreInfoEle.href = `./result.html?id=${coinId}`;
+    
+        const container = document.createElement('div');
+        //container.classList.add('')
+        container.appendChild(indexEle);
+        container.appendChild(logoEle);
+        container.appendChild(nameEle);
+        container.appendChild(shortNameEle);
+        container.appendChild(moreInfoEle);
+        
+        document.getElementById("coin-results").appendChild(container);
+    
+    }
+    
+    
+    
+    searchCoin();
